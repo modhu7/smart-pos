942e9560ae4cf405cabf4b27793e2d47215bdf77
MKRussel <russel@smartitengineering.com>
2010-09-01 11:09:58 +0600

    create new project smart-pos-admin-impl
    Signed-off-by: MKRussel <russel@smartitengineering.com>

---
+++ smart-pos-admin/smart-pos-admin-impl/src/main/java/com/smartitengineering/smartpos/admin/impl/App.java
@@ -0,0 +1,13 @@
+package com.smartitengineering.smartpos.admin.impl;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
