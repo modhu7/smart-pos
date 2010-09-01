e224d66738dfcc70171ebe4ae46a625b938148f3
MKRussel <russel@smartitengineering.com>
2010-09-01 11:09:12 +0600

    create domain class Address, GeoLocation, Organization
    Signed-off-by: MKRussel <russel@smartitengineering.com>

---
+++ smart-pos-admin/smart-pos-admin-api/src/main/java/com/smartitengineering/smartpos/admin/api/Organization.java
@@ -0,0 +1,99 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.smartitengineering.smartpos.admin.api;
+
+import com.smartitengineering.domain.AbstractPersistentDTO;
+import java.util.Date;
+import org.apache.commons.lang.StringUtils;
+import org.codehaus.jackson.annotate.JsonIgnore;
+
+
+/**
+ *
+ * @author russel
+ */
+
+public class Organization extends AbstractPersistentDTO<Organization> {
+
+
+    private String name;
+
+    private String uniqueShortName;
+    
+    private Address address;
+    //private String contactPerson;
+
+    private Date lastModifiedDate;
+
+    public Organization() {
+    }
+    
+    public Organization(String name, String uniqueShortName) {
+        this.name = name;
+        this.uniqueShortName = uniqueShortName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUniqueShortName() {
+        return uniqueShortName;
+    }
+
+    
+    public void setUniqueShortName(String uniqueShortName) {
+        this.uniqueShortName = uniqueShortName;
+    }
+
+    @JsonIgnore
+    public Date getLastModifiedDate() {
+        return lastModifiedDate;
+    }
+
+    @JsonIgnore
+    public void setLastModifiedDate(Date lastModifiedDate) {
+        this.lastModifiedDate = lastModifiedDate;
+    }
+
+    public Address getAddress() {
+        if(address==null)
+            address = new Address();
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        if(address==null)
+            return;
+        this.address = address;
+    }
+
+
+   
+
+
+//    public String getContactPerson() {
+//        if(contactPerson == null)
+//            contactPerson = "";
+//        return contactPerson;
+//    }
+//
+//    public void setContactPerson(String contactPerson) {
+//        this.contactPerson = contactPerson;
+//    }
+
+    @JsonIgnore
+    public boolean isValid(){
+        return StringUtils.isNotBlank(name) && StringUtils.isNotBlank(uniqueShortName);
+    }
+
+
+}
