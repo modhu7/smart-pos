d4f706477b43cb48923a02b655854cafdbd0f9cb
MKRussel <russel@smartitengineering.com>
2010-09-05 10:16:05 +0600

    Modify services class for newly added service
    Signed-off-by: MKRussel <russel@smartitengineering.com>

--- smart-pos-inventory/smart-pos-inventory-resource/src/main/java/com/smartitengineering/smartpos/inventory/resource/Services.java
+++ smart-pos-inventory/smart-pos-inventory-resource/src/main/java/com/smartitengineering/smartpos/inventory/resource/Services.java
@@ -5,10 +5,18 @@
 
 package com.smartitengineering.smartpos.inventory.resource;
 
+import com.smartitengineering.smartpos.inventory.api.service.EntryService;
+import com.smartitengineering.smartpos.inventory.api.service.ProductService;
 import com.smartitengineering.smartpos.inventory.api.service.StoreService;
+import com.smartitengineering.smartpos.inventory.api.service.SupplierService;
+import com.smartitengineering.smartpos.inventory.api.service.UomService;
+import com.smartitengineering.smartpos.inventory.impl.service.ProductServiceImpl;
 import com.smartitengineering.smartpos.inventory.impl.service.StoreServiceImpl;
+import com.smartitengineering.smartpos.inventory.impl.service.SupplierServiceImpl;
+import com.smartitengineering.smartpos.inventory.impl.service.UomServiceImpl;
 import com.smartitengineering.util.bean.BeanFactoryRegistrar;
 import com.smartitengineering.util.bean.annotations.Aggregator;
+import com.smartitengineering.util.bean.annotations.InjectableField;
 
 /**
  *
@@ -17,7 +25,16 @@ import com.smartitengineering.util.bean.annotations.Aggregator;
 @Aggregator(contextName = "com.smartitengineering.smartpos.inventory.api.service")
 public class Services {
 
+  @InjectableField
   private StoreService storeService;
+  @InjectableField
+  private ProductService productService;
+  @InjectableField
+  private UomService uomService;
+  @InjectableField
+  private SupplierService supplierService;
+  @InjectableField
+  private EntryService entryService;
   
 
   private Services(){    
@@ -31,6 +48,35 @@ public class Services {
   public void setStoreService(StoreService storeService) {
     this.storeService = storeService;
   }
+
+  public ProductService getProductService() {
+    //return productService;
+    return new ProductServiceImpl();
+  }
+
+  public void setProductService(ProductService productService) {
+    this.productService = productService;
+  }  
+
+  public SupplierService getSupplierService() {
+    //return supplierService;
+    return new SupplierServiceImpl();
+  }
+
+  public void setSupplierService(SupplierService supplierService) {
+    this.supplierService = supplierService;
+  }
+
+  public UomService getUomService() {
+    //return uomService;
+    return new UomServiceImpl();
+  }
+
+  public void setUomService(UomService uomService) {
+    this.uomService = uomService;
+  }
+
+
   
   private static Services services;
 
