05d18e5fc1cd1dab009706c2312dfab3ff3d27fd
MKRussel <russel@smartitengineering.com>
2010-10-11 12:05:38 +0600

    Add automatic table creation from json file.
    Signed-off-by: MKRussel <russel@smartitengineering.com>

--- smart-pos-inventory/smart-pos-inventory-client-impl/src/test/java/com/smartitengineering/smartpos/inventory/client/impl/resource/UomResourceTest.java
+++ smart-pos-inventory/smart-pos-inventory-client-impl/src/test/java/com/smartitengineering/smartpos/inventory/client/impl/resource/UomResourceTest.java
@@ -6,6 +6,7 @@
 package com.smartitengineering.smartpos.inventory.client.impl.resource;
 
 import com.google.inject.AbstractModule;
+import com.smartitengineering.smartpos.inventory.client.api.domain.UnitOfMeasurement;
 import com.smartitengineering.smartpos.inventory.client.api.resource.RootResource;
 import com.smartitengineering.smartpos.inventory.client.api.resource.UomResource;
 import com.smartitengineering.smartpos.inventory.client.api.resource.UomsResource;
@@ -50,14 +51,18 @@ public class UomResourceTest {
      * Start HBase and initialize tables
      */
 
+    System.setProperty("javax.xml.parsers.DocumentBuilderFactory",
+                       "com.sun.org.apache.xerces.internal.jaxp.DocumentBuilderFactoryImpl");
+
     TEST_UTIL.startMiniCluster();
 
-    HBaseAdmin admin = new HBaseAdmin(TEST_UTIL.getConfiguration());
-    HTableDescriptor uomTable = new HTableDescriptor("uom");
-    uomTable.addFamily(new HColumnDescriptor("self"));
-    admin.createTable(uomTable);
+//    HBaseAdmin admin = new HBaseAdmin(TEST_UTIL.getConfiguration());
+//    HTableDescriptor uomTable = new HTableDescriptor("uom");
+//    uomTable.addFamily(new HColumnDescriptor("self"));
+//    admin.createTable(uomTable);
 
-    
+    new HBaseTableGenerator(ConfigurationJsonParser.getConfigurations(AppTest.class.getClassLoader().getResourceAsStream(
+        "com/smartitengineering/cms/spi/impl/schema.json")), TEST_UTIL.getConfiguration(), true).generateTables();
 
     // DI
     Properties properties = new Properties();
@@ -109,6 +114,20 @@ public class UomResourceTest {
     uom.setUomType("Weight");
 
     UomResource uomResource = uomsResource.create(uom);
+
+    UnitOfMeasurement fetchedUom = uomResource.getUnitOfMeasurement();
+    Assert.assertNotNull(fetchedUom);
+    fetchedUom.setSymbol("Kg");
+    fetchedUom.setUomSystem("Metric");
+    fetchedUom.setUomType("Weight");
+
+    uomResource.update();
+
+    UnitOfMeasurement changedUom = uomResource.getUnitOfMeasurement();
+    Assert.assertNotSame("Kg", uom.getSymbol());
+
+    uomResource.delete();
+    
 
     Assert.assertNotNull(uomResource);
   }
--- smart-pos-server/src/main/java/com/smartitengineering/smartpos/server/GuiceServletContextListener.java
+++ smart-pos-server/src/main/java/com/smartitengineering/smartpos/server/GuiceServletContextListener.java
@@ -2,23 +2,47 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package com.smartitengineering.smartpos.server;
 
+import com.smartitengineering.dao.hbase.ddl.HBaseTableGenerator;
+import com.smartitengineering.dao.hbase.ddl.config.json.ConfigurationJsonParser;
 import com.smartitengineering.smartpos.inventory.guicebinder.Initializer;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.MasterNotRunningException;
+import com.smartitengineering.dao.impl.hbase.HBaseConfigurationFactory;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  *
  * @author russel
  */
-public class GuiceServletContextListener implements ServletContextListener{
+public class GuiceServletContextListener implements ServletContextListener {
+
+  private static Logger logger = LoggerFactory.getLogger(GuiceServletContextListener.class);
 
   @Override
   public void contextInitialized(ServletContextEvent sce) {
     Initializer.init();
+    Configuration config = HBaseConfigurationFactory.getConfigurationInstance();
+    if (config == null) {
+      config = HBaseConfigurationFactory.initConfiguration();
+    }
+
+    try {
+      new HBaseTableGenerator(ConfigurationJsonParser.getConfigurations(getClass().getClassLoader().
+          getResourceAsStream("com/smartitengineering/pos/inventory/impl/schema.json")), config, false).generateTables();
+    }
+    catch (MasterNotRunningException ex) {
+      logger.error("Master could not be found!", ex);
+    }
+    catch (Exception ex) {
+      logger.error("Could not create table!", ex);
+    }
     ServletContext servletContext = sce.getServletContext();
     servletContext.setAttribute(Initializer.class.getName(), sce);
   }
@@ -28,5 +52,4 @@ public class GuiceServletContextListener implements ServletContextListener{
     ServletContext servletContext = sce.getServletContext();
     servletContext.removeAttribute(Initializer.class.getName());
   }
-
 }
