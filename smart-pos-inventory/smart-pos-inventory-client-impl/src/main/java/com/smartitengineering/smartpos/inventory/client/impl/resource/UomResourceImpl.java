5e3779aa442f5f164a67c452e5680e53b9e34baa
MKRussel <russel@smartitengineering.com>
2010-10-12 13:23:54 +0600

    Modify Uom Resource
    Signed-off-by: MKRussel <russel@smartitengineering.com>

--- smart-pos-inventory/smart-pos-inventory-client-impl/src/main/java/com/smartitengineering/smartpos/inventory/client/impl/resource/UomResourceImpl.java
+++ smart-pos-inventory/smart-pos-inventory-client-impl/src/main/java/com/smartitengineering/smartpos/inventory/client/impl/resource/UomResourceImpl.java
@@ -7,13 +7,14 @@ package com.smartitengineering.smartpos.inventory.client.impl.resource;
 import com.smartitengineering.smartpos.inventory.client.api.resource.UomResource;
 import com.smartitengineering.smartpos.inventory.client.api.domain.UnitOfMeasurement;
 import com.smartitengineering.util.rest.atom.AbstractFeedClientResource;
-import com.smartitengineering.util.rest.client.ClientUtil;
 import com.smartitengineering.util.rest.client.Resource;
 import com.smartitengineering.util.rest.client.ResourceLink;
+import com.smartitengineering.util.rest.client.SimpleResourceImpl;
 import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.config.ClientConfig;
 import javax.ws.rs.core.MediaType;
 import org.apache.abdera.model.Feed;
+import org.apache.abdera.model.Link;
 
 /**
  *
@@ -21,11 +22,16 @@ import org.apache.abdera.model.Feed;
  */
 public class UomResourceImpl extends AbstractFeedClientResource<Resource<? extends Feed>> implements
     UomResource {
-  public static final String REL_UOM ="UnitOfMeasurement" ;
 
+  public static final String REL_UOM = "uom";
 
   public UomResourceImpl(Resource referrer, ResourceLink pageLink) {
     super(referrer, pageLink);
+    final ResourceLink altLink = getRelatedResourceUris().getFirst(Link.REL_ALTERNATE);
+    addNestedResource(REL_UOM, new SimpleResourceImpl<com.smartitengineering.smartpos.inventory.client.impl.domain.UnitOfMeasurementImpl>(
+        this, altLink.getUri(), altLink.getMimeType(),
+        com.smartitengineering.smartpos.inventory.client.impl.domain.UnitOfMeasurementImpl.class,
+        null, false, null, null));
   }
 
   @Override
@@ -37,11 +43,13 @@ public class UomResourceImpl extends AbstractFeedClientResource<Resource<? exten
   protected Resource<? extends Feed> instantiatePageableResource(ResourceLink rl) {
     return null;
   }
+
   @Override
   public UnitOfMeasurement getUnitOfMeasurement() {
     return getUnitOfMeasurement(false);
   }
-   protected UnitOfMeasurement getUnitOfMeasurement(boolean reload) {
+
+  protected UnitOfMeasurement getUnitOfMeasurement(boolean reload) {
     Resource<UnitOfMeasurement> uom = super.<UnitOfMeasurement>getNestedResource(REL_UOM);
     if (reload) {
       return uom.get();
@@ -51,11 +59,11 @@ public class UomResourceImpl extends AbstractFeedClientResource<Resource<? exten
     }
   }
 
-   public UnitOfMeasurement getUom(){
-     return getUom(false);
-   }
+  public UnitOfMeasurement getUom() {
+    return getUom(false);
+  }
 
-   protected UnitOfMeasurement getUom(boolean reload) {
+  protected UnitOfMeasurement getUom(boolean reload) {
     Resource<UnitOfMeasurement> uom = super.<UnitOfMeasurement>getNestedResource(REL_UOM);
     if (reload) {
       return uom.get();
