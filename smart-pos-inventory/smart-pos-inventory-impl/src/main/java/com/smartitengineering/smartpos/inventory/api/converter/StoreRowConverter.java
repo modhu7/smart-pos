bf1dfddbab6fb1f1fa12f582a37e295b0a77e840
MKRussel <russel@smartitengineering.com>
2010-10-13 15:41:42 +0600

    Modify ObjectRowConverter for each Domain Object
    Signed-off-by: MKRussel <russel@smartitengineering.com>

--- smart-pos-inventory/smart-pos-inventory-impl/src/main/java/com/smartitengineering/smartpos/inventory/api/converter/StoreRowConverter.java
+++ smart-pos-inventory/smart-pos-inventory-impl/src/main/java/com/smartitengineering/smartpos/inventory/api/converter/StoreRowConverter.java
@@ -2,14 +2,13 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package com.smartitengineering.smartpos.inventory.api.converter;
 
 import com.smartitengineering.dao.impl.hbase.spi.ExecutorService;
 import com.smartitengineering.dao.impl.hbase.spi.impl.AbstactObjectRowConverter;
 import com.smartitengineering.smartpos.admin.api.Address;
 import com.smartitengineering.smartpos.admin.api.GeoLocation;
-import com.smartitengineering.smartpos.inventory.api.Store;
+import com.smartitengineering.smartpos.inventory.api.PersistantStore;
 import com.smartitengineering.smartpos.inventory.api.domainid.StoreId;
 import java.util.NavigableMap;
 import org.apache.hadoop.hbase.client.Delete;
@@ -23,12 +22,10 @@ import org.slf4j.LoggerFactory;
  *
  * @author russel
  */
-public class StoreRowConverter extends AbstactObjectRowConverter<Store, StoreId> {
+public class StoreRowConverter extends AbstactObjectRowConverter<PersistantStore, StoreId> {
 
   protected final Logger logger = LoggerFactory.getLogger(StoreRowConverter.class);
-
   public static final String NAME = "name";
-  public static final String CODE = "code";
   public static final String STREET_ADDRESS = "street address";
   public static final String CITY = "city";
   public static final String STATE = "state";
@@ -36,13 +33,9 @@ public class StoreRowConverter extends AbstactObjectRowConverter<Store, StoreId>
   public static final String ZIP = "zip";
   public static final String LONGITUDE = "longitude";
   public static final String LATITUDE = "latitude";
-
   public static final byte[] FAMILY_SELF = Bytes.toBytes("self");
   public static final byte[] FAMILY_ADDRESS = Bytes.toBytes("address");
-
   public static final byte[] CELL_NAME = Bytes.toBytes(NAME);
-  public static final byte[] CELL_CODE = Bytes.toBytes(CODE);
-
   public static final byte[] CELL_STREEADDRESS = Bytes.toBytes(STREET_ADDRESS);
   public static final byte[] CELL_CITY = Bytes.toBytes(CITY);
   public static final byte[] CELL_STATE = Bytes.toBytes(STATE);
@@ -51,17 +44,13 @@ public class StoreRowConverter extends AbstactObjectRowConverter<Store, StoreId>
   public static final byte[] CELL_LONGITUDE = Bytes.toBytes(LONGITUDE);
   public static final byte[] CELL_LATITUDE = Bytes.toBytes(LATITUDE);
 
-
-  public static final String STORE_TBL_NAME = "store";
-    
-
   @Override
-  public Store rowsToObject(Result startRow, ExecutorService executorService) {
-    if(startRow.isEmpty()){
+  public PersistantStore rowsToObject(Result startRow, ExecutorService executorService) {
+    if (startRow.isEmpty()) {
       return null;
     }
 
-    final Store store = new Store();
+    final PersistantStore store = new PersistantStore();
     final Address address = new Address();
     final GeoLocation geoLocation = new GeoLocation();
 
@@ -69,9 +58,14 @@ public class StoreRowConverter extends AbstactObjectRowConverter<Store, StoreId>
 
     final NavigableMap<byte[], byte[]> self = allFamilies.get(FAMILY_SELF);
 
-    if(self != null && !self.isEmpty()){
+    if (self != null && !self.isEmpty()) {
       store.setName(Bytes.toString(self.get(CELL_NAME)));
-      
+    }
+
+    final NavigableMap<byte[], byte[]> familyAddress = allFamilies.get(FAMILY_ADDRESS);
+
+    if (familyAddress != null && !familyAddress.isEmpty()) {
+
       address.setStreetAddress(Bytes.toString(self.get(CELL_STREEADDRESS)));
       address.setCity(Bytes.toString(CELL_CITY));
       address.setState(Bytes.toString(CELL_STATE));
@@ -88,29 +82,29 @@ public class StoreRowConverter extends AbstactObjectRowConverter<Store, StoreId>
     return store;
   }
 
-
   @Override
   protected String[] getTablesToAttainLock() {
-    return new String[] {getInfoProvider().getMainTableName()};
+    return new String[]{getInfoProvider().getMainTableName()};
   }
 
   @Override
-  protected void getPutForTable(Store instance, ExecutorService service, Put put) {
-    put.add(FAMILY_SELF, CELL_NAME, Bytes.toBytes(instance.getName()));    
-    put.add(FAMILY_ADDRESS, CELL_STREEADDRESS, Bytes.toBytes(instance.getAddress().getStreetAddress()));
-    put.add(FAMILY_ADDRESS, CELL_CITY, Bytes.toBytes(instance.getAddress().getCity()));
-    put.add(FAMILY_ADDRESS, CELL_STATE, Bytes.toBytes(instance.getAddress().getState()));
-    put.add(FAMILY_ADDRESS, CELL_COUNTRY, Bytes.toBytes(instance.getAddress().getCountry()));
-    put.add(FAMILY_ADDRESS, CELL_ZIP, Bytes.toBytes(instance.getAddress().getZip()));
-    put.add(FAMILY_ADDRESS, CELL_LONGITUDE, Bytes.toBytes(instance.getAddress().getGeoLocation().getLongitude()));
-    put.add(FAMILY_ADDRESS, CELL_LATITUDE, Bytes.toBytes(instance.getAddress().getGeoLocation().getLatitude()));
+  protected void getPutForTable(PersistantStore instance, ExecutorService service, Put put) {
+    put.add(FAMILY_SELF, CELL_NAME, Bytes.toBytes(instance.getName()));
+    if (instance.getAddress() != null) {
+      put.add(FAMILY_ADDRESS, CELL_STREEADDRESS, Bytes.toBytes(instance.getAddress().getStreetAddress()));
+      put.add(FAMILY_ADDRESS, CELL_CITY, Bytes.toBytes(instance.getAddress().getCity()));
+      put.add(FAMILY_ADDRESS, CELL_STATE, Bytes.toBytes(instance.getAddress().getState()));
+      put.add(FAMILY_ADDRESS, CELL_COUNTRY, Bytes.toBytes(instance.getAddress().getCountry()));
+      put.add(FAMILY_ADDRESS, CELL_ZIP, Bytes.toBytes(instance.getAddress().getZip()));
+    }
+    if (instance.getAddress().getGeoLocation() != null) {
+      put.add(FAMILY_ADDRESS, CELL_LONGITUDE, Bytes.toBytes(instance.getAddress().getGeoLocation().getLongitude()));
+      put.add(FAMILY_ADDRESS, CELL_LATITUDE, Bytes.toBytes(instance.getAddress().getGeoLocation().getLatitude()));
+    }
   }
 
   @Override
-  protected void getDeleteForTable(Store instance, ExecutorService service, Delete delete) {
-    delete.deleteFamily(FAMILY_SELF);
+  protected void getDeleteForTable(PersistantStore instance, ExecutorService service, Delete delete) {
+    logger.info("Deleting whole store with id" + instance.getId().toString());
   }
-
-
-
 }
